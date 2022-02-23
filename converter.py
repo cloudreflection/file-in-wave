bc0657842ef41b91ad6f002b55cdfbe618a3905f
cloudreflection <39823819+cloudreflection@users.noreply.github.com>
2022-02-23 15:49:15 +0800

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,29 @@
+import wave
+import sys
+def f2w(filename: str):
+    f=wave.open(filename+'.wav','wb')
+    f.setnchannels(2)
+    f.setsampwidth(2)
+    f.setframerate(48000)
+    with open(filename,"rb") as g:
+        byte=g.read()
+    f.writeframes(byte)
+    f.close()
+    print('波形写出成功，文件名：'+filename+'.wav')
+
+def w2f(wavename: str):
+    f=wave.open(wavename,'rb')
+    with open('file_out','wb') as g:
+        g.write(f.readframes(f.getnframes()+1))
+    f.close()
+    print('文件写出成功，文件名：file_out，请自行修改原始名称和后缀名')
+
+if len(sys.argv)!=3:
+    print('文件wav互转工具 by cloudreflection\nuseage: python converter.py [option]\n   -f <文件名> 将文件转换为wav\n   -w <文件名> 将此工具生成的wav转换回文件')
+else:
+    if sys.argv[1]=='-f':
+        f2w(sys.argv[2])
+    elif sys.argv[1]=='-w':
+        w2f(sys.argv[2])
+    else:
+        print('文件wav互转工具 by cloudreflection\nuseage: python converter.py [option]\n   -f <文件名> 将文件转换为wav\n   -w <文件名> 将此工具生成的wav转换回文件')
